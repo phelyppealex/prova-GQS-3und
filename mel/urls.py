c27b389bb4893fdad6fda4df8c6aaf668c7a2225
Phelyppe Alex <phelyppesilva@gmail.com>
2023-07-15 10:00:02 -0300

    alterando url's

--- mel/urls.py
+++ mel/urls.py
@@ -28,10 +28,10 @@ urlpatterns = [
     path('listar-coletas/', views.listar_coletas, name='listar_coletas'),
     path('listar-coletas-cbv/', views.ListarColetas.as_view(), name='listar_coletas_cbv'),
 
-    path('listar-coletas/detalhes/<int:pk>', views.detalhar_coleta, name='detalhes_coleta'),
+    path('detalhes-coleta/<int:pk>', views.detalhar_coleta, name='detalhes_coleta'),
     path('listar-coletas/detalhes/<int:pk>', views.DetalharColeta.as_view(), name='detalhes_coleta_cbv'),
 
-    path('listar-coletas/deletar/<int:pk>', views.deletar_coleta, name='deletar_coleta'),
+    path('deletar-coleta/<int:pk>', views.deletar_coleta, name='deletar_coleta'),
     path('listar-coletas/deletar-cbv/<int:pk>', views.DeletarColeta.as_view(), name='deletar_coleta_cbv'),
 
     path('criar-criacao/', views.criar_criacao, name='criar_criacao'),
@@ -40,9 +40,9 @@ urlpatterns = [
     path('listar-criacoes/', views.listar_criacoes, name='listar_criacoes'),
     path('listar-criacoes-cbv/', views.ListarCriacoes.as_view(), name='listar_criacoes_cbv'),
 
-    path('listar-criacoes/detalhes/<int:pk>', views.detalhar_criacao, name='detalhes_criacao'),
+    path('detalhes-criacao/<int:pk>', views.detalhar_criacao, name='detalhes_criacao'),
     path('listar-criacoes/detalhes-cbv/<int:pk>', views.DetalharCriacao.as_view(), name='detalhes_criacao-cbv'),
 
-    path('listar-criacoes/deletar/<int:pk>', views.deletar_criacao, name='deletar_criacao'),
+    path('deletar-criacao/<int:pk>', views.deletar_criacao, name='deletar_criacao'),
     path('listar-criacoes/deletar/<int:pk>', views.DeletarCriacao.as_view(), name='detalhes_criacao_cbv'),
 ]
